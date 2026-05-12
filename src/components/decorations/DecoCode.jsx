c9998d4e6b4003504d782f55569d012f6537cc1c
Danger <monteronaelija@yahoo.com>
2026-05-12 14:14:11 +0800

    Refine DecoCode decoration styling

--- src/components/decorations/DecoCode.jsx
+++ src/components/decorations/DecoCode.jsx
@@ -5,7 +5,7 @@ export default function DecoCode() {
       <span className="cl ind"><span className="ck">const</span> {'{ data }'} = <span className="ck">await</span> supabase</span>
       <span className="cl ind2">.from(<span className="cs">&apos;leads&apos;</span>)</span>
       <span className="cl ind2">.select(<span className="cs">&apos;*&apos;</span>).eq(<span className="cs">&apos;status&apos;</span>, <span className="cs">&apos;active&apos;</span>)</span>
-      <span className="cl ind"><span className="ck">return</span> {'{ success: '}<span className="cb">true</span>{', count: data.length }'}<span className="cursor" /></span>
+      <span className="cl ind active"><span className="ck">return</span> {'{ success: '}<span className="cb">true</span>{', count: data.length }'}<span className="cursor" /></span>
       <span className="cl">{'}'}</span>
     </div>
   )
